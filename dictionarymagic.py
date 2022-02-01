0bed7f756ae784466c3ad9fcc79b622f28a70b2f
Jaya Deekshith <82016666+jayadeekshith@users.noreply.github.com>
2022-02-01 21:08:14 +0530

    Create dictionarymagic.py

---
+++ dictionarymagic.py
@@ -0,0 +1,15 @@
+# question sol 
+#https://www.hackerrank.com/challenges/py-collections-ordereddict/problem?isFullScreen=false
+#solution
+from collections import OrderedDict
+d=OrderedDict()
+for i in range(int(input())):
+    s=input().split()
+    fr=' '.join(s[:-1])
+    q=int(s[-1])
+    try:
+        d[fr]=d[fr]+q
+    except:
+        d[fr]=q
+for i,j in d.items():
+    print(i,j)
